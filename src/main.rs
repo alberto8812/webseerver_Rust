01b5f1755654715a820db027554e606fbc30fa0e
alberto8812 <cvelascosaavedra@gamil.com>
2024-12-01 22:24:13 -0500

    Agregar función de saludo personalizada y ruta para recibir parámetros en Actix Web

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -14,6 +14,12 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn greet(req: HttpRequest) -> impl Responder {
+    //obtiene el parametro de la url
+    let name: &str = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
 #[actix_web::main]
 /**
  * es un main que se ejecuta en un contexto de actix_web
@@ -26,6 +32,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello) //agrega la ruta hello
             .service(echo) //agrega la ruta echo
             .route("/hey", web::get().to(manual_hello)) //ruta que se ejecuta con el metodo get
+            .route("/greet/{name}", web::get().to(greet)) //ruta que se ejecuta con el metodo get y recibe un parametro
     })
     .bind(("127.0.0.1", 8080))?
     .run()
